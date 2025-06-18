9e0836640217ee24917faf0fe147c7c09d05afcc
KittisakSS <kittisaksonsing@gmail.com>
2025-06-18 18:39:10 +0700

    create-rating-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './components/students/students.component';
+import { StudentRatingComponent } from './components/student-rating/student-rating.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+   { path: '', component: StudentsComponent },                     // หน้าเริ่มต้น
+  { path: 'evaluate/:name', component: StudentRatingComponent }   // หน้าให้คะแนน
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/student-rating/student-rating.component.ts
+++ src/app/components/student-rating/student-rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-student-rating',
@@ -6,14 +7,24 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./student-rating.component.css']
 })
 export class StudentRatingComponent {
-  @Input() studentName: string = 'ชื่อนักเรียน'; // รับจากภายนอกในอนาคต
+  studentName: string = 'ชื่อนักเรียน';
 
-  // คะแนนที่เลือกเก็บเป็น object ตามหัวข้อ
   ratings = {
     behavior: 0,
     homework: 0,
     attendance: 0
   };
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const name = params.get('name');
+      if (name) {
+        this.studentName = name;
+      }
+    });
+  }
 
   setRating(category: string, value: number) {
     this.ratings[category] = value;
